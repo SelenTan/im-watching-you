77519335476184ff129c043e15504c9edf1a727b
SelenTan <selen.tanhanzi@gmail.com>
2026-04-28 00:44:38 +0100

    Customize personal blog

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,10 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Your Name";
-export const AUTHOR_NAME = "Your Name";
-export const AUTHOR_INITIAL = "YN";
-export const SITE_DESCRIPTION = "Your Name's personal website.";
+export const SITE_TITLE = "I'm watching you";
+export const AUTHOR_NAME = "Selene";
+export const AUTHOR_INITIAL = "ST";
+export const SITE_DESCRIPTION = "Selene's personal website is watching you back.";
 export const GENERATE_SLUG_FROM_TITLE = true;
 export const TRANSITION_API = true;
 
